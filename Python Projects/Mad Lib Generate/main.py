b004f1944e5abc4ffae6be115e167c9ba0a709b1
alamin <alamincsetpi@gmail.com>
2024-12-31 14:28:29 +0600

    Add new pdf generate and mab lib generate project

---
+++ Python Projects/Mad Lib Generate/main.py
@@ -0,0 +1,7 @@
+userName = input("prompt your username = ")
+fatherName = input("prompt your fathername = ")
+motherName = input("prompt your mothername = ")
+soceity = input("prompt your soceity = ")
+
+print(f"Your name is {userName} your father name is {fatherName}"
+      f"and your mother name is {motherName} and also your soceity is {soceity}")
