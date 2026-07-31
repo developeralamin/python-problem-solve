21c88c5bb945267d9b5f6577232e5b072772b609
alamin <alamincsetpi@gmail.com>
2026-07-31 20:44:47 +0600

    feat: update crud

--- FASTAPI/basemodel.py
+++ FASTAPI/basemodel.py
@@ -1,14 +1,69 @@
+import time
+
+import psycopg2
+
 from fastapi import FastAPI,Request
 from pydantic import BaseModel
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 class User(BaseModel):
     id: int
     name: str
     email: str
+
+class Course(BaseModel):
+    name: str
+    time: str
+
+
+while True:
+    try:
+        connection = psycopg2.connect(
+            host="localhost",
+            database="postgres",
+            user="postgres",
+            password="root",
+            cursor_factory=RealDictCursor)
+        cursor = connection.cursor()
+        print("Database connection was successful!")
+        break
+
+    except Exception as e:
+        print("Database connection failed. Retrying in 5 seconds...")
+        print(f"Error: {e}")
+        time.sleep(2)
+
+
+# //call database connection
+@app.get('/list')
+def coure_list():
+    cursor.execute("SELECT * FROM courses")
+    data = cursor.fetchall()
+    return {"success": True, "data": data}
+
+# //store data in database
+@app.post("/courses/")
+def create_course(course: Course):
+    cursor.execute("INSERT INTO courses (name, time) VALUES (%s, %s)", (course.name, course.time))
+    connection.commit()
+    return {
+        "success": True,
+        "message": "Course created successfully.",
+        "data": course
+    }
+
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello Alamin"}
+
+@app.get("/alamin")
+def read_alamin():
+    return {"message": "Hello Alamin"}
 
 @app.post("/users/")
 def create_user(user: User):
@@ -38,10 +93,3 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         }
     )
 
-@app.get("/")
-def root():
-    return {"message": "Hello Alamin"}
-
-@app.get("/alamin")
-def read_alamin():
-    return {"message": "Hello Alamin"}
