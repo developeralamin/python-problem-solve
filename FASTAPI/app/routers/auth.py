925261566f32b3e004c43202230b232c3f702f9c
alamin <alamincsetpi@gmail.com>
2026-08-04 23:01:21 +0600

    feat: add jwt oauth login system update

--- FASTAPI/app/routers/auth.py
+++ FASTAPI/app/routers/auth.py
@@ -2,24 +2,25 @@
 from fastapi import FastAPI,Request,HTTPException, Response,status,Depends,APIRouter
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session, session
-from .. database import  get_db
+from .. database import engine, get_db
 from .. import models, schemas
 from .. import oauth2
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 router = APIRouter(
     prefix="/login"
 )
 
 @router.post('/', status_code=status.HTTP_200_OK)
-def login_user(user:schemas.UserLogin, db:Session = Depends(get_db)):
-    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+def login_user(user_credentails: OAuth2PasswordRequestForm = Depends(), db:Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter(models.User.email == user_credentails.username).first()
     if not existing_user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, 
-            detail=f"User with email: {user.email} not found"
+            detail=f"User with email: {user_credentails.username} not found"
         )
     password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-    if not password_context.verify(user.password, existing_user.password):
+    if not password_context.verify(user_credentails.password, existing_user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid credentials"
